5170cb55787a450c3f1b33fb0174fd58587ba0e9
sufyan-siddiq <sufyansiddiq@gmail.com>
2024-07-11 13:16:38 +0500

    initial code

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,7 @@
-import { Header } from "@/components/layout/Header";
-import {
-  BannerSection,
-  FeatureWork,
-  GetFromUs,
-  InfiniteMovingImages,
-  ServingIndustries,
-  TestimonialsLogos,
-  WhatWeDo,
-  WhoWeAre,
-} from "@/components/Home";
-
 export default function Home() {
   return (
-    <main className="!bg-black">
-      <Header />
-      <BannerSection />
-      <TestimonialsLogos />
-      <InfiniteMovingImages />
-      <WhoWeAre />
-      {/* <ServingIndustries /> */}
-      <GetFromUs />
-      <FeatureWork />
-      <WhatWeDo />
+    <main className="text-warning h-[100vh] flex justify-center items-center text-5xl">
+      Coming Soon..
     </main>
-  );
+  )
 }
