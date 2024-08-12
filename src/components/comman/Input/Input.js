a911302d9ba97d087649f0a9528fabd914ecd2b4
sufyan-siddiq <60486936+sufyan-siddiq@users.noreply.github.com>
2024-08-12 21:43:50 +0500

    Revert "Feature/integrate contact api"

--- src/components/comman/Input/Input.js
+++ src/components/comman/Input/Input.js
@@ -1,31 +1,23 @@
-"use client";
+"use client"
 import React, { useState } from "react";
-import "./style.css";
+import "./style.css"
 
-export const Input = ({ label, name, value, onChange, className, placeholder }) => {
+export const Input = (props) => {
   const [isClicked, setIsClicked] = useState(false);
 
-  const handleFocus = () => setIsClicked(true);
-  const handleBlur = () => setIsClicked(false);
+  const handleFocus = () =>  setIsClicked(true);
+  const handleBlur = () =>  setIsClicked(false);
 
   return (
-    <div className={label ? "pt-[18px]" : ""}>
-      {label && (
-        <label className="text-[#525252] label-text mb-[8px]">
-          {label}
-        </label>
-      )}
+    <div className={props.label ? "pt-[18px]" : ""}>
+      {props.label && <label className="text-[#525252] label-text mb-[8px]">{props.label}</label>}
       <input
-        name={name}
-        placeholder={placeholder}
+        rows={props.rows}
+        placeholder={props.placeholder}
         type="text"
-        className={`${className} text-neutral focus:outline-none rounded-xl p-[16px] w-full md:w-[272px] h-[58px] bg-secondary-inputColor ${
-          isClicked ? "fade-animation" : ""
-        }`}
+        className={`${props.className} text-neutral focus:outline-none rounded-xl p-[16px] w-full md:w-[272px] h-[58px] bg-secondary-inputColor ${isClicked ? 'fade-animation' : ''}`}
         onFocus={handleFocus}
         onBlur={handleBlur}
-        value={value} 
-        onChange={onChange}
       />
     </div>
   );
