14d6b0448d57764f953619c0e22b8d31ed4510b3
MuhammadWahaj075 <wahajsiddiqui07@gmail.com>
2024-08-13 16:49:08 +0500

    Added validation

--- src/components/Contact/ContactForm/index.jsx
+++ src/components/Contact/ContactForm/index.jsx
@@ -1,39 +1,102 @@
-import { contact } from "@/utils/mockData";
-import { Button, TextArea } from "../../comman";
-import { Input } from "../../comman/Input/Input";
+"use client";
+
+import { useForm } from "react-hook-form";
+import { contact as contactMock } from "@/utils/mockData";
+import { contact } from "@/services";
+import { Input } from "@/components/comman/Input/Input";
+import { Button, TextArea } from "@/components/comman";
+import { Loader } from "@/components/comman/loader/Loader";
 
 // input
-const inputClass = "!bg-eerieBlack mt-[8px] sm:h-[66px] h-[50px] lg:w-[568px] md:w-full";
+const inputClass =
+  "!bg-eerieBlack mt-[8px] sm:h-[66px] h-[50px] lg:w-[568px] md:w-full";
 
-export const ContactForm = () => (
-  <div className="flex justify-center mt-[50px]">
-    <div className="flex flex-wrap justify-between bg-chaosBlack xl:w-[1159px] md:w-[95%] w-[95%] xl:h-[755px] h-auto rounded-[13px] md:px-[40px] md:py-[58px] py-[16px] px-[16px] ">
-      <div className="xl:w-[405px] md:text-left text-center">
-        <h1 className="title_text !text-primary-normal max-w-[737px]  md:text-left text-center">
-          {contact.title}
-        </h1>
-        <p className="description_text mt-[24px]">{contact.description}</p>
-      </div>
-      <div className="lg:w-[568px] md-w-[100%] w-[100%] xl:mt-[-18px] md:mt-[0px] flex flex-col">
-        <Input label="Email" className={inputClass} placeholder="Enter here" />
-        <Input
-          label="Subject"
-          className={inputClass}
-          placeholder="Enter here"
-        />
-        <Input
-          label="Budget"
-          className={inputClass}
-          placeholder="Select here"
-        />
-        <TextArea
-          label="Message"
-          rows={6}
-          className={"!bg-eerieBlack mt-[8px] lg:w-[568px] md:w-full"}
-          placeholder="Enter here"
-        />
-        <Button className="mt-[18px]">Submit</Button>
-      </div>
+export const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({
+    defaultValues: {
+      email: "",
+      subject: "",
+      text: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = async (data) => {
+    console.log("🚀 ~ onSubmit ~ data:", data);
+    try {
+      let response = await contact(data);
+      if (response.status === 200) {
+        alert("Message sent successfully!");
+        reset(); // Reset form fields after successful submission
+      } else {
+        alert("Failed to send the message. Please try again later.");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Failed to send the message. Please try again later.");
+    }
+  };
+
+  return (
+    <div className="flex justify-center mt-[50px]">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-wrap justify-between bg-chaosBlack xl:w-[1159px] md:w-[95%] w-[95%] xl:h-[700px] h-auto rounded-[13px] md:px-[40px] md:py-[58px] py-[16px] px-[16px]"
+      >
+        <div className="xl:w-[405px] md:text-left text-center">
+          <h1 className="title_text !text-primary-normal max-w-[737px] md:text-left text-center">
+            {contactMock.title}
+          </h1>
+          <p className="description_text mt-[24px]">
+            {contactMock.description}
+          </p>
+        </div>
+        <div className="lg:w-[568px] md-w-[100%] w-[100%] xl:mt-[-18px] md:mt-[0px] flex flex-col">
+          <Input
+            label="Email"
+            name="email"
+            className={inputClass}
+            placeholder="Email here"
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /^\S+@\S+\.\S+$/,
+                message: "Invalid email format",
+              },
+            })}
+            error={errors.email && errors.email.message}
+          />
+          <Input
+            label="Subject"
+            name="subject"
+            className={inputClass}
+            placeholder="Subject here"
+            {...register("subject", {
+              required: "Subject is required",
+            })}
+            error={errors.subject && errors.subject.message}
+          />
+          <TextArea
+            label="Message"
+            name="text"
+            rows={6}
+            className={"!bg-eerieBlack mt-[8px] lg:w-[568px] md:w-full"}
+            placeholder="Enter here"
+            {...register("text", {
+              required: "Message is required",
+            })}
+            error={errors.text && errors.text.message}
+          />
+          <Button className="mt-[18px]" type="submit" disabled={isSubmitting}>
+            {isSubmitting ? <Loader /> : "Submit"}
+          </Button>
+        </div>
+      </form>
     </div>
-  </div>
-);
+  );
+};
---
+++ src/components/comman/loader/Loader.jsx
@@ -0,0 +1,5 @@
+export const Loader = () => {
+  return (
+    <div class="animate-spin rounded-full h-6 w-6 border-t-[2px] border-b-[3px] border-white"></div>
+  );
+};
