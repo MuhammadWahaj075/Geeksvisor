72a9ecdeb0742929633c9515dea5b7b9c7e7c1d4
MuhammadWahaj075 <wahajsiddiqui07@gmail.com>
2024-08-13 20:09:58 +0500

    Fixed deploy error

--- src/app/(routes)/about/page.jsx
+++ src/app/(routes)/about/page.jsx
@@ -1,10 +1,9 @@
 import { BannerSection } from "@/components/AboutUs/BannerSection";
-import { ClientExpectation } from "@/components/AboutUs/clientExpect/ClientExpect";
 import { MeetOurTeam } from "@/components/AboutUs/MeetOurTeam";
+import { ClientExpectation, GreatIdea } from "@/components/Home";
 import { OurMission } from "@/components/AboutUs/OurMission";
 import { OurVision } from "@/components/AboutUs/OurVision";
 import { WhatWeVission } from "@/components/AboutUs/whatWeVission/WhatWeVission";
-import { GreatIdea } from "@/components/Home";
 
 
 const About = () => (
