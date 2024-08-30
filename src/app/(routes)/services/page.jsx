147109fb6fdecb9fa1287c63e140904c498a3693
Muhammad Wahaj <wahajsiddiqui07@gmail.com>
2024-08-30 20:11:12 +0500

    Fixed LinkedIn icon issue & add service page and set navbar & change  Api icon height

--- src/app/(routes)/services/page.jsx
+++ src/app/(routes)/services/page.jsx
@@ -1,6 +1,8 @@
 import { BannerSection } from "@/components/bannerSection/BannerSection"
 import { GreatIdea } from "@/components/greatIdea/GreatIdea"
 import { TestimonialsLogos, WhatPeopleSay } from "@/components/Home"
+import { Footer } from "@/components/layout/Footer"
+import { Header } from "@/components/layout/Header"
 import { Development } from "@/components/Services/development"
 import { TechnologiesWeAre } from "@/components/Services/technologiesWeAre"
 import { WhoWeAre } from "@/components/Services/whoWeAre"
@@ -8,16 +10,20 @@ import { ServingIndustries } from "@/components/servingIndustries/ServingIndustr
 
 const Services = () => {
   return (
-    <div className="max-w-[1440px] mx-auto">
-      <BannerSection />
-      <TestimonialsLogos />
-      <WhoWeAre />
-      <ServingIndustries />
-      <Development />
-      <TechnologiesWeAre />
-      <WhatPeopleSay />
-      <GreatIdea />
-    </div>
+    <>
+      <Header />
+      <div className="max-w-[1440px] mx-auto">
+        <BannerSection />
+        <TestimonialsLogos />
+        <WhoWeAre />
+        <ServingIndustries />
+        <Development />
+        <TechnologiesWeAre />
+        <WhatPeopleSay />
+        <GreatIdea />
+      </div>
+      <Footer />
+    </>
   )
 }
 
