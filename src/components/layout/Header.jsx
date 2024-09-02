cb142ab596ce2a4bb16bdb17db99c03ec58a6fa8
Muhammad Wahaj <wahajsiddiqui07@gmail.com>
2024-09-02 19:37:37 +0500

    minor changes

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -68,7 +68,7 @@ export const Header = () => {
                   className={` block border-2 border-b-secondary-light lg:border-none font-inter text-[14px] py-2 pr-4 pl-3 font-bold
                      ${pathname === item.href
                       ? "text-primary "
-                      : "text-secondary-light font-normal hover:text-neutral"
+                      : "text-secondary-light transition-all duration-150 font-normal hover:text-neutral"
                     } border-b border-transparent lg:p-0 lg:border-0`}
                 >
                   {item.navItem}
