d09c9926dc0ddceb8e8eb5ec3abde6311f172958
Muhammad Wahaj <wahajsiddiqui07@gmail.com>
2024-09-06 15:21:23 +0500

    minor changes

--- src/components/OurStrength/index.jsx
+++ src/components/OurStrength/index.jsx
@@ -13,20 +13,20 @@ export const OurStrength = () => {
                         We recognized for the following exceptional services
                     </p>
                 </div>
-                <div className="flex justify-center gap-5 lg:gap-0 px-5 !flex-col 2xl:!flex-row">
+                <div className="flex justify-center gap-5  px-5 !flex-col 2xl:!flex-row">
                     <div className="flex justify-center">
                         {OurStrengthCardData?.map((item, index) => (
                             <div className={`relative p-5 h-auto w-[100%] sm:max-w-[441.672px] md:!max-w-[900px] our_strength_card 2xl:w-auto hover:scale-105 transition duration-500 bg-secondary-bannerPill 
                             rounded-3xl shadow-lg 
                             `}>
                                 <div>
-                                    {index === 0 && <OptimizedImage src={CloudImg} className={'object-cover rounded-3xl  h-[196.66px]'} />}
+                                    {index === 0 && <OptimizedImage src={CloudImg} className={'object-cover rounded-3xl  w-full h-[196.66px]'} />}
                                     <div key={item?.id} className="p-4 flex flex-col gap-5 ">
                                         <div>
                                             <OptimizedImage src={item?.image} className='!h-[70px] w-auto object-contain' />
                                         </div>
                                         <h3 className="text-xl font-inter text-neutral font-semibold">{item?.title}</h3>
-                                        <p className="mt-2 description_text sm:w-[381.656px]">{item?.description}</p>
+                                        <p className="mt-2 description_text sm:w-[381.656px] md:w-auto ">{item?.description}</p>
                                         {/* <Button className="mt-4 absolute bottom-4 !rounded-3xl">
                                     {item?.buttonLabel}
                                 </Button> */}
